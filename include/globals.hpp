fe22a3c60e31587227aaab6027adbf4d4f4e4017
foxcaulfield <fox.caulfield@gmail.com>
2023-12-22 21:30:37 +0300

    Add headers for random number generation

--- include/globals.hpp
+++ include/globals.hpp
@@ -1,6 +1,9 @@
 #ifndef GLOBALS__HPP
 #define GLOBALS__HPP
 
+#include <cstddef>  // for std::size_t
+#include <cstdlib>  // for rand() and srand()
+#include <ctime>    // for time()
 #include <iostream>
 #include <string>
 #include <vector>
