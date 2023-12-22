2e6c12c9e48f5e8aba8ac9b619c7d6bef74d83ef
foxcaulfield <fox.caulfield@gmail.com>
2023-12-22 22:50:17 +0300

    Refactor main() function to handle exceptions and generate lorem ipsum paragraphs

--- main.cpp
+++ main.cpp
@@ -1,13 +1,56 @@
+#include "Paragraph.hpp"
 #include "globals.hpp"
 
 int main(int argc, char *argv[]) {
-    std::cout << "Args count: " << argc << std::endl;
+    try {
+        std::srand(static_cast<unsigned>(std::time(nullptr)));
+        // std::cout << "Args count: " << argc << std::endl;
 
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << std::endl;
+        if (argc != 3) {
+            throw std::invalid_argument("Usage: lorem-gen <number> <number>");
+        }
+
+        int paragraph_count = std::atoi(argv[1]);
+        int sentence_count = std::atoi(argv[2]);
+
+        if (paragraph_count <= 0 || sentence_count <= 0) {
+            throw std::invalid_argument(
+                "Program argument is not a positive integer.");
+        }
+
+        // for (int i = 0; i < argc; ++i) {
+        //     std::cout << argv[i] << std::endl;
+        // }
+
+        std::vector<Paragraph> paragraphs;
+
+        for (int i = 0; i < paragraph_count; ++i) {
+            Paragraph par;
+            for (int j = 0; j < sentence_count; ++j) {
+                par.pick_and_add_sentence();
+            }
+            paragraphs.push_back(par);
+        }
+
+        for (std::vector<Paragraph>::iterator par_iter = paragraphs.begin();
+             par_iter != paragraphs.end(); ++par_iter) {
+            std::vector<std::string> sentences = par_iter->get_sentences();
+            for (std::vector<std::string>::iterator sent_iter =
+                     sentences.begin();
+                 sent_iter != sentences.end(); ++sent_iter) {
+                std::cout << *sent_iter << " ";
+            }
+            std::cout << "\n\n";
+        }
+
+        std::cout << Paragraph::get_info() << std::endl;
+    } catch (const std::invalid_argument &e) {
+        std::cerr << "Invalid argument exception: " << e.what() << std::endl;
+    } catch (const std::exception &exception) {
+        std::cerr << "Exception caught: " << exception.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Caught unknown exception" << std::endl;
     }
-
-    std::cout << "Init" << std::endl;
 
     return 0;
 }
